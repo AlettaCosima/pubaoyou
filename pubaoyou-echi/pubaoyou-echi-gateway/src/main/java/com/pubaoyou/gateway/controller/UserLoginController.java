c83a8da2f0cf8d8c787fd64b1440c0f101492509
AlettaCosima <chentao0310@126.com>
2021-08-09 21:59:00 +0800

    init gateway

--- pubaoyou-echi/pubaoyou-echi-gateway/src/main/java/com/pubaoyou/gateway/controller/UserLoginController.java
+++ pubaoyou-echi/pubaoyou-echi-gateway/src/main/java/com/pubaoyou/gateway/controller/UserLoginController.java
@@ -1,6 +1,7 @@
 package com.pubaoyou.gateway.controller;
 
 import com.pubaoyou.gateway.service.UserLoginService;
+import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,12 +17,18 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("/user/customer/v1")
 @Slf4j
+@Api("登录相关接口文档")
 public class UserLoginController {
 
     @Resource
     private UserLoginService userLoginService;
 
     @RequestMapping("/queryUserAccount")
+    @ApiOperation(value = "查询用户",notes = "查询用户接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userName",paramType = "query",value = "用户名",dataType = "String",example = "chentao"),
+            @ApiImplicitParam(name = "password",paramType = "query",value = "密码",dataType = "String",example = "123456")
+    })
     public String queryUserAccount(@RequestParam String userName,@RequestParam String password){
         try {
             log.info("传入用户名为{}，传入密码为{}",userName,password);
